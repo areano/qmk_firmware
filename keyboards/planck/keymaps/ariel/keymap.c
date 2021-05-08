7afe51584aef105a2764543f7067f07a616d9be7
Ariel Reano <areanho@gmail.com>
2021-05-08 12:09:44 +0100

    Amost stock qwerty

--- keyboards/planck/keymaps/ariel/keymap.c
+++ keyboards/planck/keymaps/ariel/keymap.c
@@ -64,20 +64,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  `   |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | GUI  | Alt  | Ctrl |Lower |    Space    |Raise | Ctrl | Alt  |  GUI |Enter |
+ * | Caps | Ctrl | GUI  | Alt  |Lower |    Space    |Raise | Alt  | GUI  | Ctrl |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    LS,       CKC_A,   CKC_S,   CKC_D,   CKC_F,   KC_G,    KC_H,    CKC_J,   CKC_K,   CKC_L,   CKC_SCLN,RS,
-    LC,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RA,
-    KC_CAPS,  KC_LGUI, XXXXXXX, KC_LCTL, NUMS,    NAV,     NAV,     SYMBL,   KC_LCTL, XXXXXXX, KC_LGUI, XXXXXXX
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+    KC_CAPS, KC_LCTL, KC_LALT, KC_LGUI, NUMS,    NAV,     NAV,     SYMBL,   KC_LGUI, KC_LALT, KC_LCTL, XXXXXXX
 ),
-
 
 /* Adjust (Lower + Raise)
  *                      v------------------------RGB CONTROL--------------------v
